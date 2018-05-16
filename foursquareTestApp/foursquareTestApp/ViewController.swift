354db2939a6e7f90d69a558064b51e1d8b0ce855
Ja <mkm@macbook-air-mkm.local>
2018-05-16 18:26:14 +0200

    initial commit

---
+++ foursquareTestApp/foursquareTestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  foursquareTestApp
+//
+//  Created by MKM on 16.05.2018.
+//  Copyright © 2018 Tomasz Machlowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
